Fixed the scaling and scaling dict

smart_doe_bayesian_optimization/data/create_dataset.py
@@ -12,7 +12,7 @@ class DatasetManager:
         self.dataset_func = None
         self.unscaled_data = None
         self.scaled_data = None
-        self.scaling_dic = {}
+        self.scaling_dict = {}
 
     def func_create_dataset(self, dataset_func: Callable[..., torch.Tensor], num_datapoints: int, sampling_method: str = "grid", noise_level: float = 0.0, standardize: bool = True, min_max_scaling: bool = False, **kwargs):
         
@@ -25,54 +25,62 @@ class DatasetManager:
         if sampling_method == "random":
             for key, range_val in kwargs.items():
                 inputs.append(torch.rand(num_datapoints) * (range_val[1] - range_val[0]) + range_val[0])
+            inputs = torch.stack(inputs, dim=1)
         elif sampling_method == "grid":
-            grids = [torch.linspace(*range_val, steps=int(num_datapoints ** (1 / len(kwargs))))
-                     for key, range_val in kwargs.items()]
-            mesh = torch.meshgrid(*grids, indexing='ij')
-            inputs = [item.flatten() for item in mesh]
-
-        inputs = torch.stack(inputs, dim=1)
-
-        if noise_level > 0:
-            inputs = self.add_noise(inputs=inputs, noise_level=noise_level)
+            for key, range_val in kwargs.items():
+                inputs.append(torch.linspace(range_val[0], range_val[1], steps=num_datapoints))
+            inputs = torch.stack(inputs, dim=1)
 
         inputs = inputs.to(self.dtype)
 
         outputs = dataset_func(inputs)
+
+        # TODO: check for matching of input and output, otherwise do not proceed here! -> output must have as many datapoints as the input, but not same dimension
+
+        if noise_level > 0:
+            outputs = self.add_noise(outputs=outputs, noise_level=noise_level)
+
         outputs = outputs.to(self.dtype)
 
         self.unscaled_data = (inputs.clone(), outputs.clone())
 
         if standardize:
-            inputs = self.data_scaling(inputs)
-            outputs = self.data_scaling(outputs)
+            inputs, outputs = self.data_scaling(inputs, outputs)
         if min_max_scaling:
-            inputs = self.min_max_scaling(inputs)
-            outputs = self.min_max_scaling(outputs)
+            print("not implemented yet")
+    
+        print(f"Scaling dict: {self.scaling_dict}")
 
         self.scaled_data = (inputs, outputs)
 
-    def add_noise(self, inputs: torch.Tensor, noise_level: float):
-        """ Adds Gaussian noise to input data.
+    def add_noise(self, outputs: torch.Tensor, noise_level: float):
+        """ Adds Gaussian noise to output data.
 
-        :param inputs: Input data tensor.
+        :param outputs: Input data tensor.
         :param noise_level: Standard deviation of Gaussian noise to add.
-        :return: Inputs with noise added.
+        :return: outputs with noise added.
         """
-        return inputs + noise_level * torch.randn_like(inputs)
+        return outputs + noise_level * torch.randn_like(outputs)
     
-    def data_scaling(self, inputs:torch.Tensor):
-        mean = torch.mean(inputs, dim=0, keepdim=True)
-        std = torch.std(inputs, dim=0, keepdim=True)
-        self.scaling_dic['standard'] = {'mean': mean, 'std': std}
-        return (inputs - mean) / std
+    def data_scaling(self, inputs: torch.Tensor, outputs: torch.Tensor):
+        mean_inputs = torch.mean(inputs, dim=0, keepdim=True)
+        std_inputs = torch.std(inputs, dim=0, keepdim=True)
+        scaled_inputs = (inputs - mean_inputs) / std_inputs
+
+        mean_outputs = torch.mean(outputs, dim=0, keepdim=True)
+        std_outputs = torch.std(outputs, dim=0, keepdim=True)
+        scaled_outputs = (outputs - mean_outputs) / std_outputs
+
+        self.scaling_dict = {
+            'inputs': {'method': 'standardize', 'mean': mean_inputs, 'std': std_inputs},
+            'outputs': {'method': 'standardize', 'mean': mean_outputs, 'std': std_outputs}
+        }
+
+        return scaled_inputs, scaled_outputs
     
     def min_max_scaling(self, inputs: torch.Tensor):
-        """Scales the input tensor to the [0, 1] range."""
-        min_val = torch.min(inputs, dim=0, keepdim=True).values
-        max_val = torch.max(inputs, dim=0, keepdim=True).values
-        self.scaling_dic['min_max'] = {'min': min_val, 'max': max_val}
-        return (inputs - min_val) / (max_val - min_val)
+        print("min_max_scaling not implemented yet")
+        pass
     
     def transform_data(self):
         pass

smart_doe_bayesian_optimization/data/function_factory.py
@@ -1,9 +1,54 @@
 import torch
 
+
+'''
+The inputs-vector is organized in a torch.Size([n, d]) fashion:
+
+-n: number of datapoints
+-d: number of dimensions.
+
+-> so n datapoints where each has d dimensions
+
+Therefore:
+dataset_xsinx.func_create_dataset(xsinx.function_xsinx, num_datapoints=5, sampling_method="random", noise_level=0, x1_range=(0,6), x2_range=(5,10), x3_range=(100,200)) results in:
+tensor([[  1.3362,   7.6596, 130.9058],
+        [  4.9034,   9.9346, 112.4839],
+        [  5.8313,   8.9086, 154.5993],
+        [  3.8676,   5.4702, 156.1881],
+        [  4.5060,   6.5246, 162.0561]])
+torch.Size([5, 3])
+
+'''
+
 class FunctionFactory:
     @staticmethod
+    def check_dimensions(inputs, expected_shape):
+        """
+        Checks if the input tensor matches the expected shape.
+        Raises an exception if the shapes do not match.
+        """
+        if inputs.shape[1] != expected_shape:
+            raise ValueError(f"Expected input shape to have {expected_shape} dimensions, but got {inputs.shape[1]}")
+    
+    @staticmethod
     def function_xsinx(inputs):
+        
+        expected_shape = 1
 
-        # TODO: dimension check?
+        FunctionFactory.check_dimensions(inputs=inputs, expected_shape=expected_shape)
 
         return inputs * torch.sin(inputs)
+    
+    @staticmethod
+    def sum_of_sines(inputs):
+        """
+        Expects inputs tensor of shape [n, 3], where n is any number of data points.
+        Applies sine to each dimension and sums the results.
+        """
+        FunctionFactory.check_dimensions(inputs, 3)
+        result = torch.sin(inputs)
+        return result.sum(dim=1)
+    
+    @staticmethod
+    def multi_inputs(inputs):
+        pass

smart_doe_bayesian_optimization/main.py
@@ -16,30 +16,41 @@ from models.optimizer_factory import OptimizerFactory
 xsinx = FunctionFactory
 
 dataset_xsinx = DatasetManager(dtype=torch.float64)
-dataset_xsinx.func_create_dataset(xsinx.function_xsinx, num_datapoints=3, sampling_method="grid", noise_level=0, x_range=(0,5))
+dataset_xsinx.func_create_dataset(xsinx.sum_of_sines, num_datapoints=5, sampling_method="grid", noise_level=0.1, x1_range=(0,6), x2_range=(5,10), x3_range=(100,200))
 
-#Create a prior for the lengthscale
-lengthscale_prior = NormalPrior(loc=1.0, scale=0.1)
 
-# Instantiate an RBF Kernel with a lengthscale prior and ARD for 3 dimensions
-rbf_kernel = KernelFactory.create_kernel(
-    'RBF', 
-    ard_num_dims=1, 
-    lengthscale_prior=lengthscale_prior, 
-)
 
-gp_likelihood = LikelihoodFactory.create_likelihood(
-    'Gaussian',
-    noise_constraint = GreaterThan(1e-4)
-)
+# #Create a prior for the lengthscale
+# lengthscale_prior = NormalPrior(loc=1.0, scale=0.1)
 
-first_gp = BaseGPModel("SingleTaskGP", "ExactMarginalLogLikelihood", "adam", rbf_kernel, dataset_xsinx.scaled_data[0], dataset_xsinx.scaled_data[1], gp_likelihood, scaling_dic=dataset_xsinx.scaling_dic)
+# # Instantiate an RBF Kernel with a lengthscale prior and ARD for 3 dimensions
+# rbf_kernel = KernelFactory.create_kernel(
+#     'RBF', 
+#     ard_num_dims=1, 
+#     lengthscale_prior=lengthscale_prior, 
+# )
 
-scaled_X, scaled_Y = first_gp.reverse_scale()
+# gp_likelihood = LikelihoodFactory.create_likelihood(
+#     'Gaussian',
+#     noise_constraint = GreaterThan(1e-4)
+# )
 
-print(dataset_xsinx.unscaled_data[0])
-print(dataset_xsinx.unscaled_data[1])
+# first_gp = BaseGPModel("SingleTaskGP", "ExactMarginalLogLikelihood", "adam", rbf_kernel, dataset_xsinx.scaled_data[0], dataset_xsinx.scaled_data[1], gp_likelihood, scaling_dic=dataset_xsinx.scaling_dic)
 
-print(scaled_X)
+# scaled_X, scaled_Y = first_gp.reverse_scale()
 
-#first_gp.train(num_epochs=100)
+# print("second dict")
+# print(first_gp.scaling_dic)
+
+# print("unscaled ")
+# print(dataset_xsinx.unscaled_data[0])
+# print(dataset_xsinx.unscaled_data[1])
+
+# print("scaled")
+# print(dataset_xsinx.scaled_data[0])
+# print(dataset_xsinx.scaled_data[0])
+
+# print("rescaled")
+# print(scaled_X)
+
+# #first_gp.train(num_epochs=100)